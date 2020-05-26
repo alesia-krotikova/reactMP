c01d353546611b908424679259a289a417484568
Alesia Krotsikava <alesia_krotsikava@epam.com>
2020-05-26 22:04:50 +0300

    changed loader and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isDevMod = process.env.NODE_ENV === 'development';
 
@@ -27,11 +29,11 @@ module.exports = {
       },
       { test: /\.scss$/,
         use: [
-          { loader: "style-loader" },
+          (isDevMod ? "style-loader" : MiniCssExtractPlugin.loader),
           { loader: "css-modules-typescript-loader"},
           { loader: "css-loader", options: { modules: true } },
           { loader: "sass-loader" },
-        ]
+        ],
       },
     ]
   },
@@ -43,12 +45,19 @@ module.exports = {
   devtool: isDevMod ? 'eval-source-map' : 'none',
 
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    (!isDevMod && new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
+      })
+    ),
   ],
 
   optimization: !isDevMod && {
-    minimizer: [new UglifyJsPlugin()],
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
